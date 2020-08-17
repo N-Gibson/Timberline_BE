a611c21753b344fa5c9ade2a9d09a080af32f488
Noah Gibson <noah.gibson99@gmail.com>
2020-08-16 20:37:53 -0600

    Add postgress

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express();
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
+const database = require('knex')(configuration);  
 
 app.locals.title = 'timberline';
+app.use(cors());
+app.use(express.json());
+
+//Endpoints will go here
+
+module.exports = app;
