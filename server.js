c64a2ad4bd40cedee584384a680fc4836c48d128
Noah Gibson <noah.gibson99@gmail.com>
2020-08-16 19:13:17 -0600

    Add files and update server file

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
 
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Timberline BE';
-
-app.get('/', (req, res) => {
-  res.send('Timberline BE');
-});
 
 app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+  console.log(`${app.locals.title} is running on localhost:${app.get('port')}.`);
 });
