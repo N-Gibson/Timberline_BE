6e951c83a990dfba3fe68039b6502254b7e3e685
Noah Gibson <noah.gibson99@gmail.com>
2020-08-16 19:27:53 -0600

    Modify knexfile and add imports to app

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.locals.title = 'timberline';
